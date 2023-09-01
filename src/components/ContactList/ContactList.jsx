a7555f8bce3c1fffc606fd042cac383f2efbf981
Georgiy95 <urijpetrovskij@gmail.com>
2023-09-01 16:21:22 +0300

    commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import styles from "./ContactList.module.css";
 
 const ContactList = ({ contacts, filterText, handleDelete }) => {
